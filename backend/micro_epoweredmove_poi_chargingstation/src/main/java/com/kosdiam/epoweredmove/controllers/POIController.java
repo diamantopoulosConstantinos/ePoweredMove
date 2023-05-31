6a8613d59bccd13de5d6ac703df1926781acbf79
diamantopoulosConstantinos <diamantopoulosconstantinos@gmail.com>
2023-05-31 09:47:33 +0300

    Distributed Tracing Implementation
    Further configuration. Changed hostname having k8s in mind.

--- backend/micro_epoweredmove_poi_chargingstation/src/main/java/com/kosdiam/epoweredmove/controllers/POIController.java
+++ backend/micro_epoweredmove_poi_chargingstation/src/main/java/com/kosdiam/epoweredmove/controllers/POIController.java
@@ -3,6 +3,8 @@ package com.kosdiam.epoweredmove.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,9 @@ import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 @RestController
 @RequestMapping("epoweredmove/poi")
 public class POIController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(POIController.class);
+	
     private final POIService poiService;
 
     public POIController(POIService poiService) {
@@ -58,10 +63,12 @@ public class POIController {
 
     @RequestMapping(path = "allWithPlugAvailability", method = RequestMethod.GET)
     public ResponseEntity<List<POIDto>> getPOIsWithAvailability(@RequestParam(required = false) String vehicleId) {
-        var pois = poiService.getPOIsWithAvailability(vehicleId);
+    	logger.info("getPOIsWithAvailability() method started");
+    	var pois = poiService.getPOIsWithAvailability(vehicleId);
         if(pois == null) {
             throw new RecordNotFoundException("POI error occurred");
         }
+        logger.info("getPOIsWithAvailability() method finished");
         return new ResponseEntity<>(pois, HttpStatus.OK);
     }
 
